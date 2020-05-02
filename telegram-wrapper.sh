273f9851ead13a0a36e14aa5094343e14285521f
gasinvein <gasinvein@gmail.com>
2020-05-02 21:06:30 +0300

    Double pipe for OR

--- telegram-wrapper.sh
+++ telegram-wrapper.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ "${XDG_CURRENT_DESKTOP,,}" == "gnome" ] | [ "${XDG_SESSION_DESKTOP,,}" == "gnome" ]; then
+if [ "${XDG_CURRENT_DESKTOP,,}" == "gnome" ] || [ "${XDG_SESSION_DESKTOP,,}" == "gnome" ]; then
     if [ -z "$QT_QPA_PLATFORM" ]; then
         export QT_QPA_PLATFORM=xcb
     fi
